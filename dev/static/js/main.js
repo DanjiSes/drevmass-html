8068e745ad19209438ec30059ae2d9bf1fc813fd
Den <danil.savchenko.dev@gmail.com>
2021-04-24 16:08:36 +0600

    Add reviews responsive

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -6,6 +6,20 @@ $(function() {
     centerMode: true,
     centerPadding: '80px',
     slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ]
   });
 
   // Reviews popup
